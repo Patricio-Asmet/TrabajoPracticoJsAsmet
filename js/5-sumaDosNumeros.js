fa0fefc376df1c06023d0fcb41b65046f52de9cc
Patricio Asmet <patricioasmet@gmail.com>
2023-10-23 17:26:28 -0300

    ejercicio nro 5 resuelto

---
+++ js/5-sumaDosNumeros.js
@@ -0,0 +1,5 @@
+// Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+
+let numero1 = parseInt(prompt('Ingrese un numero'));
+let numero2 = parseInt(prompt('Ingrese otro numero'));
+document.write('La suma entre ambos numeros ingresados es: ' + (numero1 + numero2));
