fc5b98498132d337571ccdab35d6f209ce3efe09
Patricio Asmet <patricioasmet@gmail.com>
2023-10-26 15:08:35 -0300

    ejercicio nro 8 terminado

---
+++ js/8-divisiblePorDos.js
@@ -0,0 +1,9 @@
+// 8.- Escribe un programa que pida un número y diga si es divisible por 2.
+
+const numero1 = parseInt(prompt("Ingrese un numero"));
+
+if(numero1 % 2 === 0){
+    document.write("El número es divisible por dos.");
+}else{
+    document.write("El número no es divisible por dos.");
+}
